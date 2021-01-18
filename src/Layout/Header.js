ca86148aa01d5123987fcfac14ced13027cd150b
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-01-18 21:47:47 +0800

    header button examples
    header button examples

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -24,19 +24,15 @@ const Header = () => {
                     </Col>
                     <Col className='d-flex align-items-center'>
                         <Button variant="dark" className='mx-1'><i className='bi bi-house-fill' /></Button>
-                        <Button variant="dark" className='mx-1'>Pools</Button>
-                        <Button variant="dark" className='mx-1'>DAO</Button>
-                        <Button variant="dark" className='mx-1'>Info</Button>
+                        <Button variant="dark" className='mx-1'><bi className='bi-lg bi-cloud-plus-fill' /><div className='d-none d-sm-inline-block ml-1'>Pools</div></Button>
+                        <Button variant="dark" className='mx-1'><bi className='bi-lg bi-people-fill' /><div className='d-none d-sm-inline-block ml-1'>DAO</div></Button>
+                        <Button variant="dark" className='mx-1'><bi className='bi-lg bi-info-circle' /><div className='d-none d-sm-inline-block ml-1'>Info</div></Button>
                     </Col>
                     <Col className='header-section'>
-                        <Button variant="dark">Token Menu</Button>
-                    </Col>
-                    <Col className='header-section'>
-                        <Button variant="dark">Theme Switch</Button>
-                    </Col>
-                    {/* ICON BASED ON WALLET TYPE (METAMASK/TRUST ETC) */}
-                    <Col className='header-section'>
-                        <i class="bi bi-lg bi-wallet-fill text-light" />
+                        <Button variant="dark" className='mx-1'><bi className='bi-lg bi-file-code-fill' /></Button>
+                        <Button variant="dark" className='mx-1'><bi className='bi-lg bi-sun' /></Button>
+                        {/* ICON BASED ON WALLET TYPE (METAMASK/TRUST ETC) */}
+                        <Button variant="dark" className='mx-1'><bi className='bi-lg bi-wallet-fill' /></Button>
                     </Col>
                 </Row>
             </Container>
