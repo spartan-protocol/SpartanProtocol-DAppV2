2c1316f142f40c26b9721bb7cf518a6b9b82b4ca
Jael Romero <f1ntch@outlook.com>
2021-02-17 16:37:09 +0100

    Added Forms,Modals,Notifications,Input,Tables,Alerts Added Icon library Added Sytling

--- src/components/UI/Container/index.js
+++
@@ -1,6 +0,0 @@
-const Container = () => {
-    <div class='px-2 px-sm-3 mx-auto' style={{maxWidth:'990px'}}>
-    </div>
-}
-
-export default Container
--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
- //import Contexts from './Contexts'
-import { UseWalletProvider } from '@binance-chain/bsc-use-wallet'
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import AuthLayout from "./components/layout/Auth/Auth.js";
+import AdminLayout from "./components/layout/Admin/Admin.js";
+import "./assets/css/bd-icons.css";
+import "./assets/css/spartan-icons.css";
+
+//import Contexts from './Contexts'
+import {UseWalletProvider} from '@binance-chain/bsc-use-wallet'
 import "./assets/scss/spartan.scss";
+
 
 const rpcUrl = process.env.REACT_APP_RPC
 
 ReactDOM.render(
-  <React.StrictMode>
-    <UseWalletProvider
-      chainId={parseInt(process.env.REACT_APP_CHAIN_ID)}
-      connectors={{
-          walletconnect: { rpcUrl },
-          bsc: {},
-      }}
-    >
-      <App />
-    </UseWalletProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+    <React.StrictMode>
+        <UseWalletProvider
+            chainId={parseInt(process.env.REACT_APP_CHAIN_ID)}
+            connectors={{
+                walletconnect: {rpcUrl},
+                bsc: {},
+            }}
+        >
+        </UseWalletProvider>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/auth" render={(props) => <AuthLayout {...props} />}/>
+                <Route path="/admin" render={(props) => <AdminLayout {...props} />}/>
+                <Redirect from="/" to="/admin/dashboard"/>
+            </Switch>
+        </BrowserRouter>,
+    </React.StrictMode>,
+
+    document.getElementById("root")
+);
