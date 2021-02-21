36d345a9d645b2a94d49f3311c52206d8d63ceb1
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-02-21 18:42:41 +0800

    point to current contracts
    closes #14

--- src/utils/web3Dao.js
+++ src/utils/web3Dao.js
@@ -1,22 +1,24 @@
 import { ethers } from "ethers";
 
 import DAO from '../config/ABI/Dao.json'
-import DAOVAULT from '../config/ABI/DaoVault.json'
-import { BNB_ADDR, WBNB_ADDR, SPARTA_ADDR, getWalletProvider } from "./web3"
+// import DAOVAULT from '../config/ABI/DaoVault.json'
+import { getWalletProvider } from "./web3"
 
-const rpcUrl = process.env.REACT_APP_RPC
 const net = process.env.REACT_APP_NET
 
 // OLD CONTRACT ADDRESSES
 export const DAOv1_ADDR = net === 'testnet' ? '0x1b83a813045165c81d84b9f5d6916067b57FF9C0' : '0x04e283c9350Bab8A1243ccfc1dd9BF1Ab72dF4f0'
 
 // CURRENT CONTRACT ADDRESSES
-export const DAO_ADDR = net === 'testnet' ? '0x9329F46b82B34703b0a584e6A48A0445f6eedC53' : ''
-export const DAOVAULT_ADDR = net === 'testnet' ? '0xF629B3079584c736366b472a55f06b2ad457695A' : ''
+export const DAO_ADDR = net === 'testnet' ? '0x1b83a813045165c81d84b9f5d6916067b57FF9C0' : '0x04e283c9350Bab8A1243ccfc1dd9BF1Ab72dF4f0'
+
+// FUTURE CONTRACT ADDRESSES
+// export const DAO_ADDR = net === 'testnet' ? '' : ''
+// export const DAOVAULT_ADDR = net === 'testnet' ? '' : ''
 
 // ABI
 export const DAO_ABI = DAO.abi
-export const DAOVAULT_ABI = DAOVAULT.abi
+// export const DAOVAULT_ABI = DAOVAULT.abi
 
 // DAO - Deposit LP Tokens (Lock in DAO)
 export const deposit = async (pool, amount) => {
--- src/utils/web3Sparta.js
+++ src/utils/web3Sparta.js
@@ -1,3 +1,1 @@
-import { BNB_ADDR, WBNB_ADDR, SPARTA_ADDR } from "./web3"
-
 const net = process.env.REACT_APP_NET
