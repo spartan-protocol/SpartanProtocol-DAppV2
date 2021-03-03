bfc6e91365bad0eed21a9d5f2e232deaabacf9d6
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-03-03 18:08:43 +0800

    add contract functions to web3Sparta
    closes #32

--- src/utils/web3Sparta.js
+++ src/utils/web3Sparta.js
@@ -1,1 +1,45 @@
-const net = process.env.REACT_APP_NET
+import { ethers } from "ethers";
+
+import BASE from '../config/ABI/Base.json'
+import { getWalletProvider, SPARTA_ADDR } from "./web3"
+
+// ABI
+export const BASE_ABI = BASE.abi
+
+// GET ROUTER CONTRACT
+export const getSpartaContract = async () => {
+    let provider = getWalletProvider()
+    let contract = new ethers.Contract(SPARTA_ADDR, BASE_ABI, provider)
+    console.log(contract)
+    return contract
+}
+
+// Check whether the base contract is emitting 
+export const getEmitting = async () => {
+    let contract = await getSpartaContract()
+    const result = await contract.emitting()
+    console.log(result)
+    return result
+}
+
+// Get the 'burn' claim-rate of an asset (this should only be valid for BOND tokens now)
+export const getAdjustedClaimRate = async (assetAddress) => {
+    let contract = await getSpartaContract()
+    const result = await contract.getAdjustedClaimRate(assetAddress)
+    console.log(result)
+    return result
+}
+
+// Make a 'burn' claim (this should only be valid for BOND tokens now)
+export const claim = async (assetAddress, amount) => {
+    let provider = getWalletProvider()
+    let contract = await getSpartaContract()
+    const gPrice = await provider.getGasPrice()
+    const gLimit = await contract.estimateGas.claim(assetAddress, amount)
+    let result = await contract.claim(assetAddress, amount, {
+        gasPrice: gPrice,
+        gasLimit: gLimit
+    })
+    console.log(result)
+    return result
+}
