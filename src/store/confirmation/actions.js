3911d9e2b4141cb424c5b06371ac09bac056bb45
spieljs <spieljs@gmail.com>
2021-03-07 16:34:01 +0100

    fix tests

--- src/store/confirmation/actions.js
+++ src/store/confirmation/actions.js
@@ -13,13 +13,15 @@ export const getApproval = (tokenAddress, contractAddress) => async dispatch => 
     let contract = getTokenContract(tokenAddress)
 
     try {
-        let supply = await contract.totalSupply();
-        const gPrice = await provider.getGasPrice();
-        const gLimit = await contract.estimateGas.approve(contractAddress, supply);
+        let provider = getWalletProvider()
+        let contract = getTokenContract(tokenAddress)
+        let supply = await contract.totalSupply()
+        const gPrice = await provider.getGasPrice()
+        const gLimit = await contract.estimateGas.approve(contractAddress, supply)
         contract = await contract.approve(contractAddress, supply, {
             gasPrice: gPrice,
             gasLimit: gLimit
-        });
+        })
         
         dispatch(payloadToDispatch(Types.GET_CONTRACT, contract));
     } catch (error) {
