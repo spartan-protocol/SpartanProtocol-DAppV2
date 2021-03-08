68d9f97404f1651a03e60239c6e954f0528a9699
spieljs <spieljs@gmail.com>
2021-03-08 01:46:18 +0100

    remove functions passed to actions

--- src/utils/web3Sparta.js
+++ src/utils/web3Sparta.js
@@ -14,37 +14,3 @@ export const getSpartaContract = () => {
     let contract = new ethers.Contract(SPARTA_ADDR, BASE_ABI, provider)
     return contract
 }
-
-// --------------------------------------- HELPERS ---------------------------------------
-
-// Check whether the base contract is emitting 
-export const getEmitting = async () => {
-    let contract = getSpartaContract()
-    const result = await contract.callStatic.emitting()
-    console.log(result)
-    return result
-}
-
-// Get the 'burn' claim-rate of an asset (this should only be valid for BOND tokens now)
-export const getAdjustedClaimRate = async (assetAddress) => {
-    let contract = getSpartaContract()
-    const result = await contract.callStatic.getAdjustedClaimRate(assetAddress)
-    console.log(result)
-    return result
-}
-
-// --------------------------------------- FUNCTIONS ---------------------------------------
-
-// Make a 'burn' claim (this should only be valid for BOND tokens now)
-export const claim = async (assetAddress, amount) => {
-    let provider = getWalletProvider()
-    let contract = getSpartaContract()
-    const gPrice = await provider.getGasPrice()
-    const gLimit = await contract.estimateGas.claim(assetAddress, amount)
-    let result = await contract.claim(assetAddress, amount, {
-        gasPrice: gPrice,
-        gasLimit: gLimit
-    })
-    console.log(result)
-    return result
-}
