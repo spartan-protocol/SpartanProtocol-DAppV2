7866e479816a3b2ebc8b427590550d379c805587
Jael Romero <f1ntch@outlook.com>
2021-03-19 00:00:29 +0100

    - Join final version

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './assets/css/spartan-icons.css'
 import './app.scss'
 // import Contexts from './Contexts'
 import './assets/scss/spartan.scss'
+import './assets/css/styles.css'
 import Providers from './Providers'
 
 ReactDOM.render(<Providers />, document.getElementById('root'))
