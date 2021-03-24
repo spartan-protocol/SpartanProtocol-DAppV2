598625307506457e9ac2f26bf8f3ad4a3d88f866
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-03-24 19:56:44 +0800

    cleanup
    cleanup

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 import { Row, Button } from 'reactstrap'
 // import { getListedPools } from '../../store/utils/actions'
-import { getDaoTotalWeight } from '../../store/dao/actions'
+// import { getDaoTotalWeight } from '../../store/dao/actions'
 // import { getAddresses } from '../../utils/web3'
 
 // const addr = getAddresses()
 
-const Dashboard = () => {
-  const dispatch = useDispatch()
+const Dashboard = () => (
+  // const dispatch = useDispatch()
 
-  return (
-    <div className="content">
-      <Row>
-        <Button onClick={() => dispatch(getDaoTotalWeight())}>TEST</Button>
-      </Row>
-    </div>
-  )
-}
+  <div className="content">
+    <Row>
+      <Button onClick={() => console.log('tests')}>TEST</Button>
+    </Row>
+  </div>
+)
 
 export default Dashboard
