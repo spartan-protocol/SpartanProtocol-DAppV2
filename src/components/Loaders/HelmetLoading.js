614c4f2dcec3304d8212d27013c065a5d3a5250b
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-03-27 10:22:46 +0800

    Spinner transparent bg
    Spinner transparent bg

--- src/components/Loaders/HelmetLoading.js
+++ src/components/Loaders/HelmetLoading.js
@@ -1,5 +1,5 @@
-import React from 'react'
 import Lottie from 'lottie-react'
+import React from 'react'
 import SpartanSpinner from '../../assets/img/SpartanSpinner.json'
 
 const HelmetLoading = () => <Lottie animationData={SpartanSpinner} />
