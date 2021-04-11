f4cb6af49731aced343f9074ebcea888b5c7c990
Jael Romero <f1ntch@outlook.com>
2021-04-11 19:02:44 +0200

    - Pools styling

--- src/views/pages/Pools/Overview.js
+++ src/views/pages/Pools/Overview.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+/* eslint-disable */
+import React, { useState } from "react"
 import {
   Breadcrumb,
   Col,
@@ -8,20 +9,21 @@ import {
   Row,
   TabContent,
   TabPane,
-  UncontrolledAlert,
-} from 'reactstrap'
+  UncontrolledAlert
+} from "reactstrap"
 
-import classnames from 'classnames'
-import CardHeader from 'reactstrap/es/CardHeader'
-import Card from 'react-bootstrap/Card'
-import CardBody from 'reactstrap/es/CardBody'
-import { Line } from 'react-chartjs-2'
-import CardTitle from 'reactstrap/es/CardTitle'
-import UncontrolledTooltip from 'reactstrap/lib/UncontrolledTooltip'
-import PoolsTable from './PoolsTable'
+import classnames from "classnames"
+import CardHeader from "reactstrap/es/CardHeader"
+import Card from "react-bootstrap/Card"
+import CardBody from "reactstrap/es/CardBody"
+import { Line } from "react-chartjs-2"
+import CardTitle from "reactstrap/es/CardTitle"
+import UncontrolledTooltip from "reactstrap/lib/UncontrolledTooltip"
+import PoolsTable from "./PoolsTable"
+import Alert from "react-bootstrap/Alert"
 
 const Overview = () => {
-  const [customActiveTab, setCustomActiveTab] = useState('1')
+  const [customActiveTab, setCustomActiveTab] = useState("1")
 
   const toggleCustom = (tab) => {
     if (customActiveTab !== tab) {
@@ -30,24 +32,24 @@ const Overview = () => {
   }
   const chartTotalVolume = {
     data: (canvas) => {
-      const ctx = canvas.getContext('2d')
+      const ctx = canvas.getContext("2d")
       const gradientStroke = ctx.createLinearGradient(0, 230, 0, 50)
 
-      gradientStroke.addColorStop(0.8, 'rgb(44,41,45)')
+      gradientStroke.addColorStop(0.8, "rgb(44,41,45)")
       return {
-        labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        labels: ["JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
         datasets: [
           {
-            label: 'Data',
+            label: "Data",
             fill: true,
             backgroundColor: gradientStroke,
-            borderColor: '#fb2715',
+            borderColor: "#fb2715",
             borderWidth: 3,
             borderDash: [],
             borderDashOffset: 0.0,
-            pointBackgroundColor: '#fb2715',
-            pointBorderColor: 'rgba(0,0,0,0)',
-            pointHoverBackgroundColor: '#fb2715',
+            pointBackgroundColor: "#fb2715",
+            pointBorderColor: "rgba(0,0,0,0)",
+            pointHoverBackgroundColor: "#fb2715",
             pointBorderWidth: 20,
             pointHoverRadius: 4,
             pointHoverBorderWidth: 15,
@@ -58,26 +60,26 @@ const Overview = () => {
               125588485,
               128588485,
               140582485,
-              160582485,
-            ],
-          },
-        ],
+              160582485
+            ]
+          }
+        ]
       }
     },
     options: {
       maintainAspectRatio: false,
       legend: {
-        display: false,
+        display: false
       },
       tooltips: {
-        backgroundColor: '#f5f5f5',
-        titleFontColor: '#333',
-        bodyFontColor: '#666',
+        backgroundColor: "#f5f5f5",
+        titleFontColor: "#333",
+        bodyFontColor: "#666",
         bodySpacing: 4,
         xPadding: 12,
-        mode: 'nearest',
+        mode: "nearest",
         intersect: 0,
-        position: 'nearest',
+        position: "nearest"
       },
       responsive: true,
       scales: {
@@ -86,78 +88,78 @@ const Overview = () => {
             barPercentage: 1.6,
             gridLines: {
               drawBorder: false,
-              color: 'rgba(29,140,248,0.0)',
-              zeroLineColor: 'transparent',
+              color: "rgba(29,140,248,0.0)",
+              zeroLineColor: "transparent"
             },
             ticks: {
               suggestedMin: 60,
               suggestedMax: 125,
               padding: -60,
-              fontColor: '#1D171F',
-            },
-          },
+              fontColor: "#1D171F"
+            }
+          }
         ],
         xAxes: [
           {
             barPercentage: 1.6,
             gridLines: {
               drawBorder: true,
-              color: '#1D171F',
-              zeroLineColor: '#1D171F',
+              color: "#1D171F",
+              zeroLineColor: "#1D171F"
             },
             ticks: {
               padding: -10,
-              fontColor: '#1D171F',
-            },
-          },
-        ],
-      },
-    },
+              fontColor: "#1D171F"
+            }
+          }
+        ]
+      }
+    }
   }
 
   const chartTotalPooled = {
     data: (canvas) => {
-      const ctx = canvas.getContext('2d')
+      const ctx = canvas.getContext("2d")
       const gradientStroke = ctx.createLinearGradient(0, 230, 0, 50)
 
-      gradientStroke.addColorStop(0.8, 'rgb(44,41,45)')
+      gradientStroke.addColorStop(0.8, "rgb(44,41,45)")
       return {
-        labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+        labels: ["JUL", "AUG", "SEP", "OCT", "NOV", "DEC"],
         datasets: [
           {
-            label: 'Data',
+            label: "Data",
             fill: true,
             backgroundColor: gradientStroke,
-            borderColor: '#fb2715',
+            borderColor: "#fb2715",
             borderWidth: 3,
             borderDash: [],
             borderDashOffset: 0.0,
-            pointBackgroundColor: '#fb2715',
-            pointBorderColor: 'rgba(0,0,0,0)',
-            pointHoverBackgroundColor: '#fb2715',
+            pointBackgroundColor: "#fb2715",
+            pointBorderColor: "rgba(0,0,0,0)",
+            pointHoverBackgroundColor: "#fb2715",
             pointBorderWidth: 20,
             pointHoverRadius: 4,
             pointHoverBorderWidth: 15,
             pointRadius: 0,
-            data: [412758, 512758, 712758, 812758, 912758, 1212758],
-          },
-        ],
+            data: [412758, 512758, 712758, 812758, 912758, 1212758]
+          }
+        ]
       }
     },
     options: {
       maintainAspectRatio: false,
       legend: {
-        display: false,
+        display: false
       },
       tooltips: {
-        backgroundColor: '#f5f5f5',
-        titleFontColor: '#333',
-        bodyFontColor: '#666',
+        backgroundColor: "#f5f5f5",
+        titleFontColor: "#333",
+        bodyFontColor: "#666",
         bodySpacing: 4,
         xPadding: 12,
-        mode: 'nearest',
+        mode: "nearest",
         intersect: 0,
-        position: 'nearest',
+        position: "nearest"
       },
       responsive: true,
       scales: {
@@ -166,33 +168,33 @@ const Overview = () => {
             barPercentage: 1.6,
             gridLines: {
               drawBorder: false,
-              color: 'rgba(29,140,248,0.0)',
-              zeroLineColor: 'transparent',
+              color: "rgba(29,140,248,0.0)",
+              zeroLineColor: "transparent"
             },
             ticks: {
               suggestedMin: 60,
               suggestedMax: 125,
               padding: -60,
-              fontColor: '#1D171F',
-            },
-          },
+              fontColor: "#1D171F"
+            }
+          }
         ],
         xAxes: [
           {
             barPercentage: 1.6,
             gridLines: {
               drawBorder: true,
-              color: '#1D171F',
-              zeroLineColor: '#1D171F',
+              color: "#1D171F",
+              zeroLineColor: "#1D171F"
             },
             ticks: {
               padding: -10,
-              fontColor: '#1D171F',
-            },
-          },
-        ],
-      },
-    },
+              fontColor: "#1D171F"
+            }
+          }
+        ]
+      }
+    }
   }
 
   return (
@@ -202,33 +204,33 @@ const Overview = () => {
           <Col md={1}>
             <Breadcrumb>Pools</Breadcrumb>
           </Col>
-          <Col md={8} sm={12} className="float-left mt-3">
-            <UncontrolledAlert
-              className="alert-with-icon"
-              color="danger"
-              fade={false}
-            >
+        </Row>
+        <Row className="card-body">
+          <Col>
+            <Alert
+            className="alert-with-icon"
+            color="danger"
+            fade={false}
+          >
               <span
                 data-notify="icon"
                 className="icon-small icon-info icon-dark"
               />
-              <span data-notify="message" className="ml-n2">
+            <span data-notify="message" className="ml-n2">
                 The liquidity pools are facilitated by an automated-market-maker
                 (AMM) algorithm with liquidity-sensitive fees.
               </span>
-            </UncontrolledAlert>
-          </Col>
-        </Row>
+          </Alert></Col>
 
+        </Row>
         <Row className="card-body">
-          <Col md>
+          <Col md={6} sm={12}>
             <Card className="card-chart">
               <CardHeader>
                 <CardTitle tag="h2">
-                  <Row>
-                    <Col>
-                      <div className="text-left">
-                        Total volume{' '}
+                  <Row className="fade-in">
+                    <Col md={12} sm={12}>
+                        Total volume{" "}
                         <i
                           className="icon-small icon-info icon-dark ml-2"
                           id="tooltipAddBase"
@@ -240,24 +242,25 @@ const Overview = () => {
                         >
                           The quantity of & SPARTA you are adding to the pool.
                         </UncontrolledTooltip>
-                      </div>
                     </Col>
-                    <Col className="text-right fade-in">
-                      <div
-                        className="subtitle-amount  d-inline"
-                        style={{ fontSize: 22 }}
-                      >
-                        138.582.485
-                      </div>
-                      <div className="accent ml-1 d-inline">SPARTA</div>
-                    </Col>
-                  </Row>
+
+                      <Col md={12} sm={12} className="ml-auto">
+                        <div
+                          className="subtitle-amount  d-inline"
+                          style={{ fontSize: 22 }}
+                        >
+                          138.582.485
+                        </div>
+                        <div className="accent ml-1 d-inline">SPARTA</div>
+
+                      </Col>
+                    </Row>
                 </CardTitle>
               </CardHeader>
               <CardBody>
-                <div className="chart-area" style={{ pointerEvents: 'none' }}>
+                <div className="chart-area" style={{ pointerEvents: "none" }}>
                   <Line
-                    style={{ pointerEvents: 'none' }}
+                    style={{ pointerEvents: "none" }}
                     data={chartTotalVolume.data}
                     options={chartTotalVolume.options}
                   />
@@ -265,28 +268,27 @@ const Overview = () => {
               </CardBody>
             </Card>
           </Col>
-          <Col>
+          <Col md={6} sm={12}>
             <Card className="card-chart">
               <CardHeader>
                 <CardTitle tag="h2">
-                  <Row>
-                    <Col>
-                      <div className="text-left">
-                        Total volume{' '}
-                        <i
-                          className="icon-small icon-info icon-dark ml-2"
-                          id="tooltipAddBase"
-                          role="button"
-                        />
-                        <UncontrolledTooltip
-                          placement="right"
-                          target="tooltipAddBase"
-                        >
-                          The quantity of & SPARTA you are adding to the pool.
-                        </UncontrolledTooltip>
-                      </div>
+                  <Row className="fade-in">
+                    <Col md={12} sm={12}>
+                      Total volume{" "}
+                      <i
+                        className="icon-small icon-info icon-dark ml-2"
+                        id="tooltipAddBase"
+                        role="button"
+                      />
+                      <UncontrolledTooltip
+                        placement="right"
+                        target="tooltipAddBase"
+                      >
+                        The quantity of & SPARTA you are adding to the pool.
+                      </UncontrolledTooltip>
                     </Col>
-                    <Col className="text-right fade-in">
+
+                    <Col md={12} sm={12} className="ml-auto">
                       <div
                         className="subtitle-amount  d-inline"
                         style={{ fontSize: 22 }}
@@ -294,14 +296,15 @@ const Overview = () => {
                         $6.127.587
                       </div>
                       <div className="accent ml-1 d-inline">USD</div>
+
                     </Col>
                   </Row>
                 </CardTitle>
               </CardHeader>
               <CardBody>
-                <div className="chart-area" style={{ pointerEvents: 'none' }}>
+                <div className="chart-area" style={{ pointerEvents: "none" }}>
                   <Line
-                    style={{ pointerEvents: 'none' }}
+                    style={{ pointerEvents: "none" }}
                     data={chartTotalPooled.data}
                     options={chartTotalPooled.options}
                   />
@@ -315,12 +318,12 @@ const Overview = () => {
             <Nav className="nav-tabs-custom card-body" pills>
               <NavItem>
                 <NavLink
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                   className={classnames({
-                    active: customActiveTab === '1',
+                    active: customActiveTab === "1"
                   })}
                   onClick={() => {
-                    toggleCustom('1')
+                    toggleCustom("1")
                   }}
                 >
                   Pools overview
@@ -328,56 +331,56 @@ const Overview = () => {
               </NavItem>
               <NavItem>
                 <NavLink
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                   className={classnames({
-                    active: customActiveTab === '2',
+                    active: customActiveTab === "2"
                   })}
                   onClick={() => {
-                    toggleCustom('2')
+                    toggleCustom("2")
                   }}
                 >
                   Positions
                 </NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink
-                  style={{ cursor: 'pointer' }}
-                  className={classnames({
-                    active: customActiveTab === '3',
-                  })}
-                  onClick={() => {
-                    toggleCustom('3')
-                  }}
-                >
-                  <span className="d-none d-sm-block">Analysis</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  style={{ cursor: 'pointer' }}
-                  className={classnames({
-                    active: customActiveTab === '4',
-                  })}
-                  onClick={() => {
-                    toggleCustom('4')
-                  }}
-                >
-                  Pairs
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  style={{ cursor: 'pointer' }}
-                  className={classnames({
-                    active: customActiveTab === '5',
-                  })}
-                  onClick={() => {
-                    toggleCustom('5')
-                  }}
-                >
-                  Tokens
-                </NavLink>
-              </NavItem>
+              {/*<NavItem>*/}
+              {/*  <NavLink*/}
+              {/*    style={{ cursor: "pointer" }}*/}
+              {/*    className={classnames({*/}
+              {/*      active: customActiveTab === "3"*/}
+              {/*    })}*/}
+              {/*    onClick={() => {*/}
+              {/*      toggleCustom("3")*/}
+              {/*    }}*/}
+              {/*  >*/}
+              {/*    <span className="d-none d-sm-block">Analysis</span>*/}
+              {/*  </NavLink>*/}
+              {/*</NavItem>*/}
+              {/*<NavItem>*/}
+              {/*  <NavLink*/}
+              {/*    style={{ cursor: "pointer" }}*/}
+              {/*    className={classnames({*/}
+              {/*      active: customActiveTab === "4"*/}
+              {/*    })}*/}
+              {/*    onClick={() => {*/}
+              {/*      toggleCustom("4")*/}
+              {/*    }}*/}
+              {/*  >*/}
+              {/*    Pairs*/}
+              {/*  </NavLink>*/}
+              {/*</NavItem>*/}
+              {/*<NavItem>*/}
+              {/*  <NavLink*/}
+              {/*    style={{ cursor: "pointer" }}*/}
+              {/*    className={classnames({*/}
+              {/*      active: customActiveTab === "5"*/}
+              {/*    })}*/}
+              {/*    onClick={() => {*/}
+              {/*      toggleCustom("5")*/}
+              {/*    }}*/}
+              {/*  >*/}
+              {/*    Tokens*/}
+              {/*  </NavLink>*/}
+              {/*</NavItem>*/}
             </Nav>
 
             <TabContent activeTab={customActiveTab}>
