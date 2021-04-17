7807f44ffda4a53c96389273c165bca04423e7ad
Jael Romero <f1ntch@outlook.com>
2021-04-17 14:21:52 +0200

    - Adjusted page title and button row for all pages except liquid

--- src/views/pages/Bond/Bond.js
+++ src/views/pages/Bond/Bond.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import React, { useState } from 'react'
 import {
   Breadcrumb,
@@ -25,53 +26,69 @@ const Bond = () => {
   return (
     <>
       <div className="content">
-        <Breadcrumb>Bond & Mint</Breadcrumb>
-        <Row>
-          <Col md={10}>
-            <Row>
-              <Col md={9}>
-                <Nav className="nav-tabs-custom card-body" pills>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '1' })}
-                      onClick={() => {
-                        toggle('1')
-                      }}
-                    >
-                      Single token
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '2' })}
-                      onClick={() => {
-                        toggle('2')
-                      }}
-                    >
-                      Both tokens
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </Col>
-              <Col className="ml-5">
-                <Button type="Button" className="btn btn-danger ml-lg-n5">
-                  Proposals
-                </Button>
-                <Button type="Button" className="btn btn-primary">
-                  Claim all (2)
-                </Button>
-              </Col>
-            </Row>
-            <TabContent activeTab={activeTab}>
-              <TabPane tabId="1" className="p-3">
-                <BondTable />
-              </TabPane>
-              <TabPane tabId="2" className="p-3">
-                <NewBond />
-              </TabPane>
-            </TabContent>
+        <Row className="card-body justify-content-center">
+          <Col xs="6" xl="5">
+            <h2 className="d-inline text-title ml-1">Bond</h2>
+          </Col>
+          <Col xs="6" xl="4">
+            <Button
+              className="d-inline float-right btn btn-info mt-2 mr-2"
+            >
+              <i className="spartan-icons icon-small icon-pools icon-dark mr-2" />
+              Proposal
+            </Button>
+            <Button
+              className="d-inline float-right btn btn-info mt-2 mr-2"
+
+            >
+              <i className="spartan-icons icon-small icon-pools icon-dark mr-2" />
+              Claim all
+            </Button>
           </Col>
         </Row>
+        <Row className="justify-content-center">
+          <Col xs="12" xl="9">
+            <Row>
+              <Col md={10}>
+                <Row>
+                  <Col md={9}>
+                    <Nav className="nav-tabs-custom card-body" pills>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({ active: activeTab === '1' })}
+                          onClick={() => {
+                            toggle('1')
+                          }}
+                        >
+                          Single token
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({ active: activeTab === '2' })}
+                          onClick={() => {
+                            toggle('2')
+                          }}
+                        >
+                          Both tokens
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                  </Col>
+                </Row>
+                <TabContent activeTab={activeTab}>
+                  <TabPane tabId="1" className="p-3">
+                    <BondTable />
+                  </TabPane>
+                  <TabPane tabId="2" className="p-3">
+                    <NewBond />
+                  </TabPane>
+                </TabContent>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
       </div>
     </>
   )
--- src/views/pages/Dao/Overview.js
+++ src/views/pages/Dao/Overview.js
@@ -1,5 +1,5 @@
+/*eslint-disable*/
 import React, { useState } from 'react'
-
 import {
   Button,
   Card,
@@ -38,144 +38,146 @@ const Overview = () => {
   return (
     <>
       <div className="content">
-        <Row>
-          <Col md={2}>
-            <Breadcrumb>Dao</Breadcrumb>
+
+        <Row className="card-body justify-content-center">
+          <Col xs="6" xl="5">
+            <h2 className="d-inline text-title ml-1">Dao</h2>
           </Col>
-          <Col md={6} className="mt-3 ml-n5">
-            <Alert color="primary">
-              <span>
-                By adding liquidity to the pools you receive LP tokens. Earn
-                extra SPARTA by locking these LP tokens in the DAO
-              </span>
-            </Alert>
+          <Col xs="6" xl="4">
+
           </Col>
         </Row>
 
-        <Col md={6}> </Col>
-        <Row>
-          <Col sm={10}>
+        <Row className="justify-content-center">
+          <Col xs="12" xl="9">
             <Row>
-              <Col sm={12}>
-                <Nav className="nav-tabs-custom card-body" pills>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '1' })}
-                      onClick={() => {
-                        toggle('1')
-                      }}
+              <Col sm={10}>
+                <Row>
+                  <Col sm={12}>
+                    <Nav className="nav-tabs-custom card-body" pills>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({ active: activeTab === '1' })}
+                          onClick={() => {
+                            toggle('1')
+                          }}
+                        >
+                          Lock & earn
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({ active: activeTab === '2' })}
+                          onClick={() => {
+                            toggle('2')
+                          }}
+                        >
+                          Proposals
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                  </Col>
+                </Row>
+
+                <TabContent activeTab={activeTab}>
+                  <TabPane tabId="1" className="p-3">
+                    <br />
+                    <Card
+                      className="card-body"
+                      style={{ backgroundColor: '#1D171F' }}
                     >
-                      Lock & earn
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={classnames({ active: activeTab === '2' })}
-                      onClick={() => {
-                        toggle('2')
-                      }}
-                    >
-                      Proposals
-                    </NavLink>
-                  </NavItem>
-                </Nav>
+                      <CardBody>
+                        <Row>
+                          <Col md={3} xs={12} className="mb-n4">
+                            <h2 className="mt-3">
+                              Claim rewards
+                              <i
+                                className="icon-small icon-info icon-dark ml-2"
+                                id="tooltipAddBase"
+                                role="button"
+                              />
+                              <UncontrolledTooltip
+                                placement="right"
+                                target="tooltipAddBase"
+                              >
+                                The quantity of & SPARTA you are adding to the pool.
+                              </UncontrolledTooltip>
+                            </h2>
+                          </Col>
+                          <Col md={2}>
+                            <div className="card-text">Rewards</div>
+                          </Col>
+                          <Col md={2}>
+                            <div className="card-text">DAO weight</div>
+                          </Col>
+                          <Col md={2}>
+                            <div className="card-text">Latest harvest</div>
+                          </Col>
+                          <Col md={2} className="ml-auto mr-2 mt-2">
+                            <Button
+                              type="Button"
+                              className="btn btn-primary"
+                              onClick={() => dispatch(daoHarvest())}
+                            >
+                              Harvest
+                            </Button>
+                          </Col>
+                        </Row>
+                        <Row>
+                          <Col md={3} />
+                          <Col md={2}>
+                            <div className="title-card mt-n2 ">
+                              <div className="subtitle-amount mt-n4">
+                                {formatFromWei(dao.harvestAmount.toString())}
+                                <i className="icon-extra-small icon-spinner icon-dark ml-1" />
+                              </div>
+                            </div>
+                          </Col>
+                          <Col md={2}>
+                            <div className="title-card mt-n4">
+                              {daoVault.memberWeight > 0 &&
+                              formatFromUnits(
+                                BN(daoVault.memberWeight.toString())
+                                  .div(daoVault.daoTotalWeight.toString())
+                                  .times(100),
+                              )}
+                              {daoVault.memberWeight <= 0 && 'Not a DAO member'}
+                            </div>
+                          </Col>
+                          <Col md={2}>
+                            <div className="title-card mt-n4">XXX</div>
+                          </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+                    <div className="page-header">
+                      Lock & earn{' '}
+                      <i
+                        className="icon-small icon-info icon-dark ml-2"
+                        id="tooltipAddBase"
+                        role="button"
+                      />
+                      <UncontrolledTooltip
+                        placement="right"
+                        target="tooltipAddBase"
+                      >
+                        The quantity of & SPARTA you are adding to the pool.
+                      </UncontrolledTooltip>
+                    </div>
+                    <br />
+                    <LockEarn />
+                  </TabPane>
+                  <TabPane tabId="2" className="p-3">
+                    <Proposals />
+                  </TabPane>
+                </TabContent>
               </Col>
             </Row>
-
-            <TabContent activeTab={activeTab}>
-              <TabPane tabId="1" className="p-3">
-                <br />
-                <Card
-                  className="card-body"
-                  style={{ backgroundColor: '#1D171F' }}
-                >
-                  <CardBody>
-                    <Row>
-                      <Col md={3} xs={12} className="mb-n4">
-                        <h2 className="mt-3">
-                          Claim rewards
-                          <i
-                            className="icon-small icon-info icon-dark ml-2"
-                            id="tooltipAddBase"
-                            role="button"
-                          />
-                          <UncontrolledTooltip
-                            placement="right"
-                            target="tooltipAddBase"
-                          >
-                            The quantity of & SPARTA you are adding to the pool.
-                          </UncontrolledTooltip>
-                        </h2>
-                      </Col>
-                      <Col md={2}>
-                        <div className="card-text">Rewards</div>
-                      </Col>
-                      <Col md={2}>
-                        <div className="card-text">DAO weight</div>
-                      </Col>
-                      <Col md={2}>
-                        <div className="card-text">Latest harvest</div>
-                      </Col>
-                      <Col md={2} className="ml-auto mr-2 mt-2">
-                        <Button
-                          type="Button"
-                          className="btn btn-primary"
-                          onClick={() => dispatch(daoHarvest())}
-                        >
-                          Harvest
-                        </Button>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col md={3} />
-                      <Col md={2}>
-                        <div className="title-card mt-n2 ">
-                          <div className="subtitle-amount mt-n4">
-                            {formatFromWei(dao.harvestAmount.toString())}
-                            <i className="icon-extra-small icon-spinner icon-dark ml-1" />
-                          </div>
-                        </div>
-                      </Col>
-                      <Col md={2}>
-                        <div className="title-card mt-n4">
-                          {daoVault.memberWeight > 0 &&
-                            formatFromUnits(
-                              BN(daoVault.memberWeight.toString())
-                                .div(daoVault.daoTotalWeight.toString())
-                                .times(100),
-                            )}
-                          {daoVault.memberWeight <= 0 && 'Not a DAO member'}
-                        </div>
-                      </Col>
-                      <Col md={2}>
-                        <div className="title-card mt-n4">XXX</div>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-                <div className="page-header">
-                  Lock & earn{' '}
-                  <i
-                    className="icon-small icon-info icon-dark ml-2"
-                    id="tooltipAddBase"
-                    role="button"
-                  />
-                  <UncontrolledTooltip
-                    placement="right"
-                    target="tooltipAddBase"
-                  >
-                    The quantity of & SPARTA you are adding to the pool.
-                  </UncontrolledTooltip>
-                </div>
-                <br />
-                <LockEarn />
-              </TabPane>
-              <TabPane tabId="2" className="p-3">
-                <Proposals />
-              </TabPane>
-            </TabContent>
           </Col>
         </Row>
+
+
+
       </div>
     </>
   )
