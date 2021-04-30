f563c4cd08601682f5a06ff322e919925f910e38
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-04-30 10:51:25 +0800

    cleanup for PR
    cleanup for PR

--- src/components/Supply/Supply.js
+++ src/components/Supply/Supply.js
@@ -1,4 +1,3 @@
-/* eslint-disable*/
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -27,11 +26,8 @@ const Supply = () => {
   const addr = getAddresses()
   const [openedCollapseThree, setopenedCollapseThree] = React.useState(false)
 
-
-
   return (
     <>
-
       <Button
         id="PopoverClick"
         type="Button"
@@ -43,7 +39,8 @@ const Supply = () => {
       </Button>
 
       <UncontrolledPopover
-        trigger="legacy" rootClose
+        trigger="legacy"
+        rootClose
         placement="bottom"
         target="PopoverClick"
       >
