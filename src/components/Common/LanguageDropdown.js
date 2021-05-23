413d2c9e56adac5dfd0d9976d7632202b303b142
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-05-23 13:12:54 +0800

    lint cleanup
    lint cleanup

--- src/components/Common/LanguageDropdown.js
+++ src/components/Common/LanguageDropdown.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react'
-import {
-  Row,
-  Col,
-  Button,
-  UncontrolledPopover,
-  PopoverHeader,
-  PopoverBody,
-} from 'reactstrap'
+import { Row, Col, Button, UncontrolledPopover, PopoverHeader, PopoverBody } from 'reactstrap'
 
 // i18n
 import i18n from '../../i18n'
@@ -33,169 +26,151 @@ import uaFlag from '../../assets/flags/ua.svg'
 import zaFlag from '../../assets/flags/za.svg'
 
 const getEnFlag = () => {
-  const enCountry = navigator.language.slice(-2)
-  if (enCountry === 'AU') {
-    return auFlag
-  }
-  if (enCountry === 'US') {
-    return usFlag
-  }
-  return gbFlag
+	const enCountry = navigator.language.slice(-2)
+	if (enCountry === 'AU') {
+		return auFlag
+	}
+	if (enCountry === 'US') {
+		return usFlag
+	}
+	return gbFlag
 }
 
 const locales = [
-  {
-    id: 'en',
-    name: 'English',
-    flag: getEnFlag(),
-  },
-  {
-    id: 'zh',
-    name: '中国人',
-    flag: cnFlag,
-  },
-  {
-    id: 'es',
-    name: 'Español',
-    flag: esFlag,
-  },
-  {
-    id: 'fr',
-    name: 'Français',
-    flag: frFlag,
-  },
-  {
-    id: 'af',
-    name: 'Afrikaans',
-    flag: zaFlag,
-  },
-  {
-    id: 'bn',
-    name: 'বাংলা',
-    flag: inFlag,
-  },
-  {
-    id: 'de',
-    name: 'Deutsche',
-    flag: deFlag,
-  },
-  {
-    id: 'el',
-    name: 'Ελληνικά',
-    flag: grFlag,
-  },
-  {
-    id: 'fa',
-    name: 'فارسی',
-    flag: irFlag,
-  },
-  {
-    id: 'hi',
-    name: 'हिंदी',
-    flag: inFlag,
-  },
-  {
-    id: 'it',
-    name: 'Italiano',
-    flag: itFlag,
-  },
-  {
-    id: 'nl',
-    name: 'Nederlands',
-    flag: nlFlag,
-  },
-  {
-    id: 'pa',
-    name: 'ਪੰਜਾਬੀ',
-    flag: inFlag,
-  },
-  {
-    id: 'pl',
-    name: 'Polskie',
-    flag: plFlag,
-  },
-  {
-    id: 'pt',
-    name: 'Português',
-    flag: ptFlag,
-  },
-  {
-    id: 'ro',
-    name: 'Română',
-    flag: roFlag,
-  },
-  {
-    id: 'ru',
-    name: 'Pусский',
-    flag: ruFlag,
-  },
-  {
-    id: 'sv',
-    name: 'Svenska',
-    flag: seFlag,
-  },
-  {
-    id: 'uk',
-    name: 'Український',
-    flag: uaFlag,
-  },
+	{
+		id: 'en',
+		name: 'English',
+		flag: getEnFlag(),
+	},
+	{
+		id: 'zh',
+		name: '中国人',
+		flag: cnFlag,
+	},
+	{
+		id: 'es',
+		name: 'Español',
+		flag: esFlag,
+	},
+	{
+		id: 'fr',
+		name: 'Français',
+		flag: frFlag,
+	},
+	{
+		id: 'af',
+		name: 'Afrikaans',
+		flag: zaFlag,
+	},
+	{
+		id: 'bn',
+		name: 'বাংলা',
+		flag: inFlag,
+	},
+	{
+		id: 'de',
+		name: 'Deutsche',
+		flag: deFlag,
+	},
+	{
+		id: 'el',
+		name: 'Ελληνικά',
+		flag: grFlag,
+	},
+	{
+		id: 'fa',
+		name: 'فارسی',
+		flag: irFlag,
+	},
+	{
+		id: 'hi',
+		name: 'हिंदी',
+		flag: inFlag,
+	},
+	{
+		id: 'it',
+		name: 'Italiano',
+		flag: itFlag,
+	},
+	{
+		id: 'nl',
+		name: 'Nederlands',
+		flag: nlFlag,
+	},
+	{
+		id: 'pa',
+		name: 'ਪੰਜਾਬੀ',
+		flag: inFlag,
+	},
+	{
+		id: 'pl',
+		name: 'Polskie',
+		flag: plFlag,
+	},
+	{
+		id: 'pt',
+		name: 'Português',
+		flag: ptFlag,
+	},
+	{
+		id: 'ro',
+		name: 'Română',
+		flag: roFlag,
+	},
+	{
+		id: 'ru',
+		name: 'Pусский',
+		flag: ruFlag,
+	},
+	{
+		id: 'sv',
+		name: 'Svenska',
+		flag: seFlag,
+	},
+	{
+		id: 'uk',
+		name: 'Український',
+		flag: uaFlag,
+	},
 ]
 
 const getLocale = () => locales?.filter((x) => x.id === i18n.languages[0])[0]
 
 const LanguageDropdown = () => {
-  // const [lng, setLng] = useState(i18n.languages[0])
-  const [flag, setFlag] = useState(getLocale()?.flag || locales[4]?.flag)
+	// const [lng, setLng] = useState(i18n.languages[0])
+	const [flag, setFlag] = useState(getLocale()?.flag || locales[4]?.flag)
 
-  const changeLanguageAction = (lang) => {
-    i18n.changeLanguage(lang)
-    // setLng(lang)
-    setFlag(getLocale().flag)
-  }
+	const changeLanguageAction = (lang) => {
+		i18n.changeLanguage(lang)
+		// setLng(lang)
+		setFlag(getLocale().flag)
+	}
 
-  return (
-    <>
-      <Button
-        id="PopoverClick1"
-        type="Button"
-        className="btn-round btn-transparent btn-icon ml-1"
-        href="#"
-      >
-        <img src={flag} alt="flag" className="icon-small mt-2" />
-        {/* <i className="icon-small icon-lang icon-dark mt-2" /> */}
-      </Button>
-      <UncontrolledPopover
-        trigger="legacy"
-        rootclose="true"
-        placement="bottom"
-        target="PopoverClick1"
-      >
-        <PopoverHeader className="mt-2">Language</PopoverHeader>
-        <PopoverBody>
-          <Row>
-            {locales.map((x) => (
-              <Col xs={6} key={x.id} className="pl-3 pr-1">
-                <Button
-                  onClick={() => changeLanguageAction(x.id)}
-                  className="btn-transparent"
-                >
-                  <span className="output-card">
-                    <img
-                      src={x.flag}
-                      alt="Spartan"
-                      height="12"
-                      className="mr-2"
-                    />
-                    {x.name}
-                  </span>
-                </Button>
-              </Col>
-            ))}
-          </Row>
-        </PopoverBody>
-      </UncontrolledPopover>
-    </>
-  )
+	return (
+		<>
+			<Button id="PopoverClick1" type="Button" className="btn-round btn-transparent btn-icon ml-1" href="#">
+				<img src={flag} alt="flag" className="icon-small mt-2" />
+				{/* <i className="icon-small icon-lang icon-dark mt-2" /> */}
+			</Button>
+			<UncontrolledPopover trigger="legacy" rootclose="true" placement="bottom" target="PopoverClick1">
+				<PopoverHeader className="mt-2">Language</PopoverHeader>
+				<PopoverBody>
+					<Row>
+						{locales.map((x) => (
+							<Col xs={6} key={x.id} className="pl-3 pr-1">
+								<Button onClick={() => changeLanguageAction(x.id)} className="btn-transparent">
+									<span className="output-card">
+										<img src={x.flag} alt="Spartan" height="12" className="mr-2" />
+										{x.name}
+									</span>
+								</Button>
+							</Col>
+						))}
+					</Row>
+				</PopoverBody>
+			</UncontrolledPopover>
+		</>
+	)
 }
 
 export default LanguageDropdown
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Container, UncontrolledTooltip } from 'reactstrap'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { Container, UncontrolledTooltip, Row, Col } from 'reactstrap'
 import Sparta from '../../assets/icons/coin_sparta.svg'
 import gitbookSVG from '../../assets/icons/icon-gitbook-dark.svg'
 import twitterSVG from '../../assets/icons/icon-twitter-dark.svg'
@@ -12,159 +10,71 @@ import redditSVG from '../../assets/icons/icon-reddit-dark.svg'
 import './Footer.scss'
 
 const footerStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
 }
 
 const Footer = () => (
-  <footer
-    className="footer footer-default"
-    style={{ backgroundColor: '#25212d' }}
-  >
-    <Container fluid>
-      <Row style={footerStyle}>
-        <Col xs={12} sm={4} className="mt-2">
-          <div className="text-footer" style={{ color: 'white' }}>
-            A Spartan Community Project
-          </div>
-        </Col>
-        <Col xs={12} sm={4} className="mt-2">
-          <div className="text-footer">
-            <div style={{ height: '100%', textAlign: 'center' }}>
-              <img className="mr-2" src={Sparta} alt="Logo" height="32" />
-              Spartan Protocol
-            </div>
-          </div>
-        </Col>
-        <Col xs={12} sm={4} className="mt-2">
-          {/* <div className="copyright ml-4">
-              <a
-                href="https://twitter.com/SpartanProtocol"
-                target="_blank"
-                rel="noreferrer"
-                id="footer-twitter"
-              >
-                <i className="icon-small icon-twitter icon-dark mr-3 zoomsm" />
-                <UncontrolledTooltip target="footer-twitter" placement="top">
-                  Twitter
-                </UncontrolledTooltip>
-              </a>
-              <a
-                href="https://github.com/spartan-protocol"
-                target="_blank"
-                rel="noreferrer"
-                id="footer-github"
-              >
-                <i className="icon-small icon-github icon-dark mr-3 zoomsm" />
-                <UncontrolledTooltip target="footer-github" placement="top">
-                  Github
-                </UncontrolledTooltip>
-              </a>
-              <a
-                href="https://t.me/SpartanProtocolOrg"
-                target="_blank"
-                rel="noreferrer"
-                id="footer-telegram"
-              >
-                <i className="icon-small icon-telegram icon-dark mr-3 zoomsm" />
-                <UncontrolledTooltip target="footer-telegram" placement="top">
-                  Telegram
-                </UncontrolledTooltip>
-              </a>
-              <a
-                href="https://spartanprotocol.medium.com/"
-                target="_blank"
-                rel="noreferrer"
-                id="footer-medium"
-              >
-                <i className="icon-small icon-mediums icon-dark mr-3 zoomsm" />
-                <UncontrolledTooltip target="footer-medium" placement="top">
-                  Medium
-                </UncontrolledTooltip>
-              </a>
-              <a
-                href="https://www.reddit.com/r/SpartanProtocol/"
-                target="_blank"
-                rel="noreferrer"
-                id="footer-reddit"
-              ></a>
-                <i className="icon-small icon-reddit icon-dark mr-3 zoomsm" />
-                <UncontrolledTooltip target="footer-reddit" placement="top">
-                  Reddit
-                </UncontrolledTooltip> */}
-          <div className="copyright">
-            <a
-              href="https://docs.spartanprotocol.org/"
-              target="_blank"
-              rel="noreferrer"
-              id="footer-gitbook"
-            >
-              <img src={gitbookSVG} alt="gitbook" height={24} />
-              <UncontrolledTooltip target="footer-gitbook" placement="top">
-                Gitbook
-              </UncontrolledTooltip>
-            </a>
-            <a
-              href="https://twitter.com/SpartanProtocol"
-              target="_blank"
-              rel="noreferrer"
-              id="footer-twitter"
-            >
-              <img src={twitterSVG} alt="twitter" height={24} />
-              <UncontrolledTooltip target="footer-twitter" placement="top">
-                Twitter
-              </UncontrolledTooltip>
-            </a>
-            <a
-              href="https://github.com/spartan-protocol"
-              target="_blank"
-              rel="noreferrer"
-              id="footer-github"
-            >
-              <img src={githubSVG} alt="twitter" height={24} />
-              <UncontrolledTooltip target="footer-github" placement="top">
-                Github
-              </UncontrolledTooltip>
-            </a>
-            <a
-              href="https://t.me/SpartanProtocolOrg"
-              target="_blank"
-              rel="noreferrer"
-              id="footer-telegram"
-            >
-              <img src={telegramSVG} alt="twitter" height={24} />
-              <UncontrolledTooltip target="footer-telegram" placement="top">
-                Telegram
-              </UncontrolledTooltip>
-            </a>
-            <a
-              href="https://spartanprotocol.medium.com"
-              target="_blank"
-              rel="noreferrer"
-              id="footer-medium"
-            >
-              <img src={mediumSVG} alt="twitter" height={24} />
-              <UncontrolledTooltip target="footer-medium" placement="top">
-                Medium
-              </UncontrolledTooltip>
-            </a>
-            <a
-              href="https://www.reddit.com/r/SpartanProtocol"
-              target="_blank"
-              rel="noreferrer"
-              id="footer-reddit"
-            >
-              <img src={redditSVG} alt="twitter" height={24} />
-              <UncontrolledTooltip target="footer-reddit" placement="top">
-                Reddit
-              </UncontrolledTooltip>
-            </a>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  </footer>
+	<footer className="footer footer-default" style={{ backgroundColor: '#25212d' }}>
+		<Container fluid>
+			<Row style={footerStyle}>
+				<Col xs={12} sm={4} className="mt-2">
+					<div className="text-footer" style={{ color: 'white' }}>
+						A Spartan Community Project
+					</div>
+				</Col>
+				<Col xs={12} sm={4} className="mt-2">
+					<div className="text-footer">
+						<div style={{ height: '100%', textAlign: 'center' }}>
+							<img className="mr-2" src={Sparta} alt="Logo" height="32" />
+							Spartan Protocol
+						</div>
+					</div>
+				</Col>
+				<Col xs={12} sm={4} className="mt-2">
+					<div className="copyright">
+						<a href="https://docs.spartanprotocol.org/" target="_blank" rel="noreferrer" id="footer-gitbook">
+							<img src={gitbookSVG} alt="gitbook" height={24} />
+							<UncontrolledTooltip target="footer-gitbook" placement="top">
+								Gitbook
+							</UncontrolledTooltip>
+						</a>
+						<a href="https://twitter.com/SpartanProtocol" target="_blank" rel="noreferrer" id="footer-twitter">
+							<img src={twitterSVG} alt="twitter" height={24} />
+							<UncontrolledTooltip target="footer-twitter" placement="top">
+								Twitter
+							</UncontrolledTooltip>
+						</a>
+						<a href="https://github.com/spartan-protocol" target="_blank" rel="noreferrer" id="footer-github">
+							<img src={githubSVG} alt="twitter" height={24} />
+							<UncontrolledTooltip target="footer-github" placement="top">
+								Github
+							</UncontrolledTooltip>
+						</a>
+						<a href="https://t.me/SpartanProtocolOrg" target="_blank" rel="noreferrer" id="footer-telegram">
+							<img src={telegramSVG} alt="twitter" height={24} />
+							<UncontrolledTooltip target="footer-telegram" placement="top">
+								Telegram
+							</UncontrolledTooltip>
+						</a>
+						<a href="https://spartanprotocol.medium.com" target="_blank" rel="noreferrer" id="footer-medium">
+							<img src={mediumSVG} alt="twitter" height={24} />
+							<UncontrolledTooltip target="footer-medium" placement="top">
+								Medium
+							</UncontrolledTooltip>
+						</a>
+						<a href="https://www.reddit.com/r/SpartanProtocol" target="_blank" rel="noreferrer" id="footer-reddit">
+							<img src={redditSVG} alt="twitter" height={24} />
+							<UncontrolledTooltip target="footer-reddit" placement="top">
+								Reddit
+							</UncontrolledTooltip>
+						</a>
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	</footer>
 )
 
 export default Footer
