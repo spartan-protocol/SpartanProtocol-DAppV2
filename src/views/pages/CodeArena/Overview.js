e91c7c824a70db6bd2f3b3f7979274b3daf4ab3f
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-06-14 20:49:17 +0800

    extend codeArena donation page
    extend codeArena donation page

--- src/views/pages/CodeArena/Overview.js
+++ src/views/pages/CodeArena/Overview.js
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from 'react'
-import { Col, Row } from 'reactstrap'
 
 import { useTranslation } from 'react-i18next'
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBCardHeader,
+  MDBCardText,
+  MDBCardTitle,
+  MDBCol,
+  MDBInputGroup,
+  MDBInputGroupElement,
+  MDBInputGroupText,
+  MDBProgress,
+  MDBProgressBar,
+  MDBRow,
+} from 'mdb-react-ui-kit'
+import { useDispatch } from 'react-redux'
+import axios from 'axios'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { ReactComponent as BnbIcon } from '../../../assets/icons/BNB.svg'
 import { getNetwork } from '../../../utils/web3'
+import { communityWalletHoldings } from '../../../store/sparta/actions'
+import { useSparta } from '../../../store/sparta/selector'
+import { BN, formatFromWei } from '../../../utils/bigNumber'
+import { useWeb3 } from '../../../store/web3/selector'
 
 const Overview = () => {
+  const dispatch = useDispatch()
   const { t } = useTranslation()
+  const sparta = useSparta()
+  const wallet = useWallet()
+  const web3 = useWeb3()
   const [network, setnetwork] = useState(getNetwork())
   const [trigger0, settrigger0] = useState(0)
   const getData = () => {
@@ -23,34 +50,244 @@ const Overview = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [trigger0])
 
+  const [bnbPrice, setbnbPrice] = useState(0)
+  const [trigger1, settrigger1] = useState(0)
+  const getHoldings = async () => {
+    dispatch(communityWalletHoldings(wallet))
+    const _bnbPrice = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd',
+    )
+    setbnbPrice(_bnbPrice.data.binancecoin.usd)
+  }
+  useEffect(() => {
+    if (trigger1 === 0) {
+      getHoldings()
+    }
+    const timer = setTimeout(() => {
+      getHoldings()
+      settrigger1(trigger1 + 1)
+    }, 7500)
+    return () => clearTimeout(timer)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [trigger1])
+
+  const [totalUSD, settotalUSD] = useState(0)
+  useEffect(() => {
+    const _spartaUSD = BN(sparta.communityWallet?.sparta).times(
+      web3.spartaPrice,
+    )
+    const _bnbUSD = BN(sparta.communityWallet?.bnb).times(bnbPrice)
+    settotalUSD(
+      BN(_spartaUSD)
+        .plus(_bnbUSD)
+        .plus(sparta.communityWallet?.busd)
+        .plus(sparta.communityWallet?.usdt)
+        .toString(),
+    )
+  }, [
+    sparta.communityWallet?.sparta,
+    sparta.communityWallet?.bnb,
+    sparta.communityWallet?.busd,
+    sparta.communityWallet?.usdt,
+    bnbPrice,
+    web3.spartaPrice,
+  ])
+
+  const [totalWidth, settotalWidth] = useState(0)
+  useEffect(() => {
+    settotalWidth(
+      BN(totalUSD).div(100000000000000000000000).times(100).toString(),
+    )
+  }, [totalUSD])
+
+  const [progColor, setprogColor] = useState('primary')
+  useEffect(() => {
+    let _progColor = 'primary'
+    if (totalWidth >= 80) {
+      _progColor = 'green'
+    } else if (totalWidth >= 30) {
+      _progColor = 'info'
+    }
+    setprogColor(_progColor)
+  }, [totalWidth])
+
+  const [selectedAsset, setselectedAsset] = useState(false)
+
   return (
     <>
       <div className="content">
-        <Row className="row-480">
-          <Col xs="12">
+        <MDBRow className="row-480">
+          <MDBCol xs="12">
             <div className="card-480 my-3">
               <h2 className="text-title-small mb-0 mr-3">{t('codeArena')}</h2>
             </div>
-          </Col>
-        </Row>
-        <Row className="row-480">
-          <Col xs="12">DONATION TARGET BAR</Col>
-          <Col xs="12">CODEARENA INFO</Col>
-        </Row>
-        {network.chainId === 56 && (
-          <>
-            <Row className="row-480">
-              <Col xs="12">DONATE BUTTON</Col>
-            </Row>
-          </>
-        )}
-        {network.chainId !== 56 && (
-          <Row className="row-480">
-            <Col xs="12">
-              Switch the DApp and your wallet to mainnet to donate
-            </Col>
-          </Row>
-        )}
+          </MDBCol>
+        </MDBRow>
+        <MDBRow className="row-480">
+          <MDBCol>
+            <MDBCard className="card-480">
+              <MDBCardHeader>
+                <MDBCardTitle>Donate to the CodeArena Campaign</MDBCardTitle>
+              </MDBCardHeader>
+              <MDBCardBody>
+                {network.chainId === 56 && (
+                  <>
+                    <MDBCardText onClick={() => setselectedAsset('bnb')}>
+                      <MDBRow>
+                        <MDBCol size="auto" className="pr-0">
+                          <BnbIcon height="35" />
+                        </MDBCol>
+                        <MDBCol>
+                          <MDBRow>
+                            <MDBCol
+                              size="12"
+                              className="float-left ml-n4 output-card"
+                            >
+                              BNB - Binance Coin BEP20 Token
+                              <div className="description">
+                                Your Wallet:{' '}
+                                {formatFromWei(sparta.communityWallet?.userBnb)}
+                              </div>
+                            </MDBCol>
+                          </MDBRow>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBCardText>
+
+                    <MDBCardText>
+                      <MDBRow>
+                        <MDBCol size="auto" className="pr-0">
+                          <BnbIcon height="35" />
+                        </MDBCol>
+                        <MDBCol>
+                          <MDBRow>
+                            <MDBCol
+                              size="12"
+                              className="float-left ml-n4 output-card"
+                            >
+                              BUSD - Binance-Peg BUSD Token
+                              <div className="description">
+                                Your Wallet:{' '}
+                                {formatFromWei(
+                                  sparta.communityWallet?.userBusd,
+                                )}
+                              </div>
+                            </MDBCol>
+                          </MDBRow>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBCardText>
+
+                    <MDBCardText>
+                      <MDBRow>
+                        <MDBCol size="auto" className="pr-0">
+                          <BnbIcon height="35" />
+                        </MDBCol>
+                        <MDBCol>
+                          <MDBRow>
+                            <MDBCol
+                              size="12"
+                              className="float-left ml-n4 output-card"
+                            >
+                              USDT - Binance-Peg USD-T Token
+                              <div className="description">
+                                Your Wallet:{' '}
+                                {formatFromWei(
+                                  sparta.communityWallet?.userUsdt,
+                                )}
+                              </div>
+                            </MDBCol>
+                          </MDBRow>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBCardText>
+
+                    <MDBCardText className="my-3">
+                      <MDBInputGroup>
+                        <MDBInputGroupElement
+                          type="number"
+                          placeholder="Donation Amount..."
+                        />
+                        <MDBInputGroupText>{selectedAsset}</MDBInputGroupText>
+                      </MDBInputGroup>
+                    </MDBCardText>
+                  </>
+                )}
+
+                <MDBCardText className="mt-3 mb-2">
+                  <MDBProgress height="15">
+                    <MDBProgressBar bgColor={progColor} width={totalWidth}>
+                      ${formatFromWei(totalUSD, 0)}
+                    </MDBProgressBar>
+                    <MDBProgressBar bgColor="black" width={100 - totalWidth}>
+                      {'<-- Wallet Holdings'}
+                    </MDBProgressBar>
+                  </MDBProgress>
+                </MDBCardText>
+
+                <MDBCardText>
+                  <MDBProgress height="30">
+                    <MDBProgressBar width="30">$30K (Min)</MDBProgressBar>
+                    <MDBProgressBar bgColor="black" width="0.25" />
+                    <MDBProgressBar bgColor="info" width="50">
+                      $80K (Target)
+                    </MDBProgressBar>
+                    <MDBProgressBar bgColor="black" width="0.25" />
+                    <MDBProgressBar bgColor="green" width="20">
+                      $100K
+                    </MDBProgressBar>
+                  </MDBProgress>
+                </MDBCardText>
+              </MDBCardBody>
+              <MDBCardFooter>
+                {network.chainId === 56 && (
+                  <>
+                    <MDBBtn justify="center">Donate</MDBBtn>
+                  </>
+                )}
+                {network.chainId !== 56 && (
+                  <>
+                    <MDBCardText>
+                      Switch the DApp and your wallet to mainnet to contribute
+                      funds
+                    </MDBCardText>
+                  </>
+                )}
+              </MDBCardFooter>
+            </MDBCard>
+          </MDBCol>
+          <MDBCol>
+            <MDBCard className="card-480">
+              <MDBCardHeader>
+                <MDBCardTitle>Crowdfunded CodeArena Bounty Target</MDBCardTitle>
+              </MDBCardHeader>
+              <MDBCardBody>
+                <MDBCardText>
+                  The Spartan Protocol V2 contracts will undergo a{' '}
+                  <a
+                    href="https://code423n4.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    CodeArena audit
+                  </a>{' '}
+                  in the next available slot. The scope of contracts to be
+                  audited has a recomended bounty requirement of $80k USD. Being
+                  a community built and run project; there is a drive to
+                  crowdfund the bounty. The audit will go ahead whether or not
+                  the target is reached, but raising $80k will ensure the entire
+                  suite of contracts are audited!
+                </MDBCardText>
+              </MDBCardBody>
+              <MDBCardHeader>
+                <MDBCardTitle>Recent Donations</MDBCardTitle>
+              </MDBCardHeader>
+              <MDBCardBody>
+                <MDBCardText>Placeholder</MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
       </div>
     </>
   )
--- src/views/pages/Home/NewPool.js
+++ src/views/pages/Home/NewPool.js
@@ -278,173 +278,183 @@ const NewPool = () => {
       >
         <MDBModalDialog centered>
           <MDBModalContent>
-            <MDBModalHeader>
-              <MDBModalTitle>{t('createPool')}</MDBModalTitle>
-              <MDBBtn
-                onClick={() => setShowModal(false)}
-                color="link"
-                className="my-auto"
-              >
-                <i className="icon-small icon-close" />
-              </MDBBtn>
-            </MDBModalHeader>
-            <MDBModalBody>
-              {network.chainId === 56 &&
-                trustWalletIndex.data?.includes(addrInput?.value) &&
-                tokenInfo && (
-                  <div className="text-sm-label-alt text-center">
-                    <img
-                      src={tokenIcon}
-                      height="45px"
-                      alt="tokenIcon"
-                      className="mr-2"
+            {network.chainId === 97 && (
+              <>
+                <MDBModalHeader>
+                  <MDBModalTitle>{t('createPool')}</MDBModalTitle>
+                  <MDBBtn
+                    onClick={() => setShowModal(false)}
+                    color="link"
+                    className="my-auto"
+                  >
+                    <i className="icon-small icon-close" />
+                  </MDBBtn>
+                </MDBModalHeader>
+                <MDBModalBody>
+                  {network.chainId === 56 &&
+                    trustWalletIndex.data?.includes(addrInput?.value) &&
+                    tokenInfo && (
+                      <div className="text-sm-label-alt text-center">
+                        <img
+                          src={tokenIcon}
+                          height="45px"
+                          alt="tokenIcon"
+                          className="mr-2"
+                        />
+                        {`${tokenInfo.symbol} | ${tokenInfo.decimals} decimals | ${tokenInfo.name}`}
+                      </div>
+                    )}
+                  <InputGroup className="mt-2">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>Address</InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="addrInput"
+                      placeholder="0x..."
+                      type="text"
+                      inputMode="decimal"
+                      pattern="^[0-9]*[.,]?[0-9]*$"
+                      autoComplete="off"
+                      autoCorrect="off"
                     />
-                    {`${tokenInfo.symbol} | ${tokenInfo.decimals} decimals | ${tokenInfo.name}`}
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText className="p-1">
+                        {addrValid ? (
+                          <ValidIcon fill="green" height="30" width="30" />
+                        ) : (
+                          <InvalidIcon fill="red" height="30" width="30" />
+                        )}
+                      </InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  <div className="text-sm-label-alt pb-2 text-center">
+                    Input a valid token address (18 decimal BEP20 asset listed
+                    in the{' '}
+                    <a
+                      href="https://github.com/trustwallet/assets/tree/master/blockchains/smartchain"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      TrustWallet repo
+                    </a>
+                    )
                   </div>
-                )}
-              <InputGroup className="mt-2">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>Address</InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  id="addrInput"
-                  placeholder="0x..."
-                  type="text"
-                  inputMode="decimal"
-                  pattern="^[0-9]*[.,]?[0-9]*$"
-                  autoComplete="off"
-                  autoCorrect="off"
-                />
-                <InputGroupAddon addonType="append">
-                  <InputGroupText className="p-1">
-                    {addrValid ? (
-                      <ValidIcon fill="green" height="30" width="30" />
-                    ) : (
-                      <InvalidIcon fill="red" height="30" width="30" />
-                    )}
-                  </InputGroupText>
-                </InputGroupAddon>
-              </InputGroup>
-              <div className="text-sm-label-alt pb-2 text-center">
-                Input a valid token address (18 decimal BEP20 asset listed in
-                the{' '}
-                <a
-                  href="https://github.com/trustwallet/assets/tree/master/blockchains/smartchain"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  TrustWallet repo
-                </a>
-                )
-              </div>
-              <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>Input</InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  id="spartaInput"
-                  placeholder=""
-                  type="number"
-                  inputMode="decimal"
-                  autoComplete="off"
-                  autoCorrect="off"
-                  disabled={!addrValid}
-                />
-                <InputGroupAddon addonType="append">
-                  <InputGroupText className="p-1">SPARTA</InputGroupText>
-                </InputGroupAddon>
-                <InputGroupAddon addonType="append">
-                  <InputGroupText className="p-1">
-                    {spartaValid ? (
-                      <ValidIcon fill="green" height="30" width="30" />
-                    ) : (
-                      <InvalidIcon fill="red" height="30" width="30" />
-                    )}
-                  </InputGroupText>
-                </InputGroupAddon>
-              </InputGroup>
-              <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>Input</InputGroupText>
-                </InputGroupAddon>
-                <Input
-                  id="tokenInput"
-                  placeholder=""
-                  type="number"
-                  inputMode="decimal"
-                  autoComplete="off"
-                  autoCorrect="off"
-                  disabled={!addrValid}
-                />
-                <InputGroupAddon addonType="append">
-                  <InputGroupText className="p-1">{tokenSymbol}</InputGroupText>
-                </InputGroupAddon>
-                <InputGroupAddon addonType="append">
-                  <InputGroupText className="p-1">
-                    {tokenValid ? (
-                      <ValidIcon fill="green" height="30" width="30" />
-                    ) : (
-                      <InvalidIcon fill="red" height="30" width="30" />
-                    )}
-                  </InputGroupText>
-                </InputGroupAddon>
-              </InputGroup>
-              <div className="text-sm-label-alt pb-2 text-center">
-                Minimum of 10,000 SPARTA required
-              </div>
+                  <InputGroup>
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>Input</InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="spartaInput"
+                      placeholder=""
+                      type="number"
+                      inputMode="decimal"
+                      autoComplete="off"
+                      autoCorrect="off"
+                      disabled={!addrValid}
+                    />
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText className="p-1">SPARTA</InputGroupText>
+                    </InputGroupAddon>
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText className="p-1">
+                        {spartaValid ? (
+                          <ValidIcon fill="green" height="30" width="30" />
+                        ) : (
+                          <InvalidIcon fill="red" height="30" width="30" />
+                        )}
+                      </InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  <InputGroup>
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>Input</InputGroupText>
+                    </InputGroupAddon>
+                    <Input
+                      id="tokenInput"
+                      placeholder=""
+                      type="number"
+                      inputMode="decimal"
+                      autoComplete="off"
+                      autoCorrect="off"
+                      disabled={!addrValid}
+                    />
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText className="p-1">
+                        {tokenSymbol}
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <InputGroupAddon addonType="append">
+                      <InputGroupText className="p-1">
+                        {tokenValid ? (
+                          <ValidIcon fill="green" height="30" width="30" />
+                        ) : (
+                          <InvalidIcon fill="red" height="30" width="30" />
+                        )}
+                      </InputGroupText>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  <div className="text-sm-label-alt pb-2 text-center">
+                    Minimum of 10,000 SPARTA required
+                  </div>
 
-              <div className="output-card text-center mb-2">
-                1 SPARTA = {priceInSparta()} {tokenSymbol}
-                <br />1 {tokenSymbol} = {priceInToken()} SPARTA
-                <br />1 {tokenSymbol} = ~${priceinUSD()} USD
-              </div>
-              <FormGroup>
-                <div className="text-center">
-                  <CustomInput
-                    type="switch"
-                    id="inputConfirmRatio"
-                    label="Confirm ratio! Avoid getting rekt!"
-                    checked={ratioConfirm}
-                    onChange={() => setRatioConfirm(!ratioConfirm)}
-                  />
-                </div>
-              </FormGroup>
-            </MDBModalBody>
-            <MDBModalFooter>
-              {wallet?.account && spartaInput?.value > 0 && (
-                <Approval
-                  tokenAddress={addr.spartav2}
-                  symbol="SPARTA"
-                  walletAddress={wallet.account}
-                  contractAddress={addr.poolFactory}
-                  txnAmount={convertToWei(spartaInput?.value)}
-                  assetNumber="1"
-                />
-              )}
-              <MDBCol size="12" className="hide-if-siblings">
-                <MDBBtn
-                  block
-                  className="btn-fill btn-primary"
-                  disabled={!ratioConfirm || !formValid}
-                  onClick={() => handleSubmit()}
-                >
-                  {t('confirm')}
-                </MDBBtn>
-              </MDBCol>
-              {wallet?.account &&
-                tokenInput?.value > 0 &&
-                addrInput?.value !== addr.bnb && (
-                  <Approval
-                    tokenAddress={addrInput?.value}
-                    symbol={tokenSymbol}
-                    walletAddress={wallet.account}
-                    contractAddress={addr.poolFactory}
-                    txnAmount={convertToWei(tokenInput?.value)}
-                    assetNumber="2"
-                  />
-                )}
-            </MDBModalFooter>
-            {network.chainId !== 97 && <WrongNetwork />}
+                  <div className="output-card text-center mb-2">
+                    1 SPARTA = {priceInSparta()} {tokenSymbol}
+                    <br />1 {tokenSymbol} = {priceInToken()} SPARTA
+                    <br />1 {tokenSymbol} = ~${priceinUSD()} USD
+                  </div>
+                  <FormGroup>
+                    <div className="text-center">
+                      <CustomInput
+                        type="switch"
+                        id="inputConfirmRatio"
+                        label="Confirm ratio! Avoid getting rekt!"
+                        checked={ratioConfirm}
+                        onChange={() => setRatioConfirm(!ratioConfirm)}
+                      />
+                    </div>
+                  </FormGroup>
+                </MDBModalBody>
+                <MDBModalFooter>
+                  {wallet?.account && spartaInput?.value > 0 && (
+                    <Approval
+                      tokenAddress={addr.spartav2}
+                      symbol="SPARTA"
+                      walletAddress={wallet.account}
+                      contractAddress={addr.poolFactory}
+                      txnAmount={convertToWei(spartaInput?.value)}
+                      assetNumber="1"
+                    />
+                  )}
+                  <MDBCol size="12" className="hide-if-siblings">
+                    <MDBBtn
+                      block
+                      className="btn-fill btn-primary"
+                      disabled={!ratioConfirm || !formValid}
+                      onClick={() => handleSubmit()}
+                    >
+                      {t('confirm')}
+                    </MDBBtn>
+                  </MDBCol>
+                  {wallet?.account &&
+                    tokenInput?.value > 0 &&
+                    addrInput?.value !== addr.bnb && (
+                      <Approval
+                        tokenAddress={addrInput?.value}
+                        symbol={tokenSymbol}
+                        walletAddress={wallet.account}
+                        contractAddress={addr.poolFactory}
+                        txnAmount={convertToWei(tokenInput?.value)}
+                        assetNumber="2"
+                      />
+                    )}
+                </MDBModalFooter>
+              </>
+            )}
+            {network.chainId !== 97 && (
+              <MDBModalBody>
+                <WrongNetwork />
+              </MDBModalBody>
+            )}
           </MDBModalContent>
         </MDBModalDialog>
       </MDBModal>
