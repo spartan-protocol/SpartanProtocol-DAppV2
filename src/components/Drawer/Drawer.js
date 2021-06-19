be93a41ecf1891587267b7ba1237980fc974b53d
Patrick <pdronk@bol.com>
2021-06-19 13:26:24 +0200

    fixed the drawer

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.scss'
+import RecentTxns from '../RecentTxns/RecentTxns'
+
+const Drawer = (props) => {
+  let drawerClasses = 'side-drawer'
+  if (props.show) {
+    drawerClasses = 'side-drawer open'
+  }
+  return (
+    <div className={drawerClasses}>
+      <RecentTxns />
+    </div>
+  )
+}
+
+export default Drawer
