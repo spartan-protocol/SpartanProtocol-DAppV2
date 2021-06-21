aa44e5b6c6cf0b964b729665d0dc61a538738044
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-06-21 15:11:45 +0800

    fix lint
    fix lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-const App = () => (
-  <Router />
-)
+const App = () => <Router />
 
 export default React.memo(App)
