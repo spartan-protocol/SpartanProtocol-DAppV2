a51732e7df3ca3e03bc8620e7d3fbc565cc19788
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-06-22 16:15:00 +0800

    Pools Overview converted
    Pools Overview converted

--- src/views/pages/Pools/Overview.js
+++ src/views/pages/Pools/Overview.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import {
-  MDBCol,
-  MDBRow,
-  MDBTabs,
-  MDBTabsItem,
-  MDBTabsLink,
-} from 'mdb-react-ui-kit'
+import { Col, Row, Tabs, Tab } from 'react-bootstrap'
 import LiqAdd from './LiqAdd'
 import LiqRemove from './LiqRemove'
 import LiqBond from './LiqBond'
@@ -39,72 +33,44 @@ const Overview = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [trigger0])
 
-  const toggle = (tab) => {
-    if (activeTab !== tab) setActiveTab(tab)
-  }
-
   return (
     <>
       <div className="content">
-        <MDBRow className="row-480">
-          <MDBCol xs="12">
+        <Row className="row-480">
+          <Col xs="12">
             <div className="card-480 my-3">
-              <h2 className="text-title-small mb-0 mr-3">{t('liquidity')}</h2>
+              <h2 className="text-title-small mb-0 me-3">{t('liquidity')}</h2>
               <NewPool />
               {pool.poolDetails.length > 0 && <SharePool />}
             </div>
-          </MDBCol>
-        </MDBRow>
+          </Col>
+        </Row>
         {network.chainId === 97 && (
           <>
-            <MDBRow className="row-480">
-              <MDBCol size="12">
-                <MDBTabs className="nav-tabs-custom card-480 mb-3">
-                  <MDBTabsItem>
-                    <MDBTabsLink
-                      active={activeTab === '1'}
-                      onClick={() => {
-                        toggle('1')
-                      }}
-                    >
-                      <span className="">{t('add')}</span>
-                    </MDBTabsLink>
-                  </MDBTabsItem>
-                  <MDBTabsItem>
-                    <MDBTabsLink
-                      active={activeTab === '2'}
-                      onClick={() => {
-                        toggle('2')
-                      }}
-                    >
-                      <span className="">{t('remove')}</span>
-                    </MDBTabsLink>
-                  </MDBTabsItem>
-                  <MDBTabsItem>
-                    <MDBTabsLink
-                      active={activeTab === '4'}
-                      onClick={() => {
-                        toggle('4')
-                      }}
-                    >
-                      <span className="">{t('bond')}</span>
-                    </MDBTabsLink>
-                  </MDBTabsItem>
-                </MDBTabs>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow className="row-480">
-              {pool.poolDetails.length > 0 && (
-                <>
-                  {activeTab === '1' && <LiqAdd />}
-                  {activeTab === '2' && <LiqRemove />}
-                  {activeTab === '4' && <LiqBond />}
-                </>
-              )}
-              <MDBCol className="card-480">
+            <Row className="row-480">
+              <Col xs="12">
+                <Tabs
+                  activeKey={activeTab}
+                  onSelect={(k) => setActiveTab(k)}
+                  className="mb-3 card-480"
+                >
+                  <Tab eventKey="1" title={t('add')}>
+                    {pool.poolDetails.length > 0 && <LiqAdd />}
+                  </Tab>
+                  <Tab eventKey="2" title={t('remove')}>
+                    {pool.poolDetails.length > 0 && <LiqRemove />}
+                  </Tab>
+                  <Tab eventKey="4" title={t('bond')}>
+                    {pool.poolDetails.length > 0 && <LiqBond />}
+                  </Tab>
+                </Tabs>
+              </Col>
+            </Row>
+            <Row className="row-480">
+              <Col className="card-480">
                 {pool.poolDetails.length <= 0 && <HelmetLoading />}
-              </MDBCol>
-            </MDBRow>
+              </Col>
+            </Row>
           </>
         )}
         {network.chainId !== 97 && <WrongNetwork />}
