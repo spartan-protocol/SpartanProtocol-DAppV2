b10032ae9de06760d2abf7f1c097ed357ba0450f
jacobostel <81117226+jacobostel@users.noreply.github.com>
2021-09-28 04:23:40 +0200

    Add files via upload

--- src/views/pages/Swap/Swap.js
+++ src/views/pages/Swap/Swap.js
@@ -12,6 +12,7 @@ import {
   Badge,
   OverlayTrigger,
   Form,
+  Popover,
 } from 'react-bootstrap'
 import { useWeb3React } from '@web3-react/core'
 import AssetSelect from '../../../components/AssetSelect/AssetSelect'
@@ -720,43 +721,100 @@ const Swap = () => {
                                   </Col>
                                 </Row>
                                 <Row className="my-1">
-                                  <Col>
-                                    <InputGroup className="m-0">
-                                      <InputGroup.Text id="assetSelect1">
-                                        <AssetSelect
-                                          priority="1"
-                                          filter={getFilter()}
-                                          onClick={handleConfClear}
-                                        />
-                                      </InputGroup.Text>
-                                      <FormControl
-                                        className="text-end ms-0"
-                                        type="number"
-                                        placeholder={`${t('sell')}...`}
-                                        id="swapInput1"
-                                        autoComplete="off"
-                                        autoCorrect="off"
-                                      />
-                                      <InputGroup.Text
-                                        role="button"
-                                        tabIndex={-1}
-                                        onKeyPress={() => clearInputs()}
-                                        onClick={() => clearInputs()}
-                                      >
-                                        <Icon
-                                          icon="close"
-                                          size="10"
-                                          fill="grey"
-                                        />
-                                      </InputGroup.Text>
-                                    </InputGroup>
-                                    <div className="text-end text-sm-label pt-1">
-                                      ~$
-                                      {swapInput1?.value
-                                        ? formatFromWei(getInput1USD(), 2)
-                                        : '0.00'}
-                                    </div>
-                                  </Col>
+                                  {typeof wallet.account === 'undefined' ? (
+                                    <>
+                                      <Col>
+                                        <OverlayTrigger
+                                          placement="auto"
+                                          overlay={
+                                            <Popover>
+                                              <Popover.Header />
+                                              <Popover.Body>
+                                                {t('connectWalletFirst')}
+                                              </Popover.Body>
+                                            </Popover>
+                                          }
+                                        >
+                                          <InputGroup className="m-0">
+                                            <InputGroup.Text id="assetSelect1">
+                                              <AssetSelect
+                                                priority="1"
+                                                filter={getFilter()}
+                                                onClick={handleConfClear}
+                                              />
+                                            </InputGroup.Text>
+                                            <FormControl
+                                              className="text-end ms-0"
+                                              type="number"
+                                              placeholder={`${t('sell')}...`}
+                                              id="swapInput1"
+                                              autoComplete="off"
+                                              autoCorrect="off"
+                                            />
+                                            <InputGroup.Text
+                                              role="button"
+                                              tabIndex={-1}
+                                              onKeyPress={() => clearInputs()}
+                                              onClick={() => clearInputs()}
+                                            >
+                                              <Icon
+                                                icon="close"
+                                                size="10"
+                                                fill="grey"
+                                              />
+                                            </InputGroup.Text>
+                                          </InputGroup>
+                                        </OverlayTrigger>
+                                        <div className="text-end text-sm-label pt-1">
+                                          ~$
+                                          {swapInput1?.value
+                                            ? formatFromWei(getInput1USD(), 2)
+                                            : '0.00'}
+                                        </div>
+                                      </Col>
+                                    </>
+                                  ) : (
+                                    <>
+                                      <Col>
+                                        <InputGroup className="m-0">
+                                          <InputGroup.Text id="assetSelect1">
+                                            <AssetSelect
+                                              priority="1"
+                                              filter={getFilter()}
+                                              onClick={handleConfClear}
+                                            />
+                                          </InputGroup.Text>
+                                          <FormControl
+                                            className="text-end ms-0"
+                                            type="number"
+                                            placeholder={`${t('sell')}...`}
+                                            id="swapInput1"
+                                            autoComplete="off"
+                                            autoCorrect="off"
+                                          />
+                                          <InputGroup.Text
+                                            role="button"
+                                            tabIndex={-1}
+                                            onKeyPress={() => clearInputs()}
+                                            onClick={() => clearInputs()}
+                                          >
+                                            <Icon
+                                              icon="close"
+                                              size="10"
+                                              fill="grey"
+                                            />
+                                          </InputGroup.Text>
+                                        </InputGroup>
+
+                                        <div className="text-end text-sm-label pt-1">
+                                          ~$
+                                          {swapInput1?.value
+                                            ? formatFromWei(getInput1USD(), 2)
+                                            : '0.00'}
+                                        </div>
+                                      </Col>
+                                    </>
+                                  )}
                                 </Row>
                               </Card.Body>
                             </Card>
