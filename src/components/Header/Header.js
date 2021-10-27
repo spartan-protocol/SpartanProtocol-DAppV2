0427f7410e4b217838c0ba8ba1aa2c80cbf06719
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-10-27 13:52:40 +0800

    fix wrong breakpoint menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,14 +13,14 @@ import { Icon } from '../Icons/icons'
 const Header = () => (
   <Navbar className="header" fixed="top">
     <Container fluid>
-      <div className="d-lg-none">
+      <div className="d-xl-none">
         <Sidebar />
         <Link to="/" className="navbar-brand ms-2">
           <Icon icon="spartav2" className="my-auto" size="35" />
           <h4 className="d-none d-sm-inline ms-2">Spartan Protocol</h4>
         </Link>
       </div>
-      <div className="d-none d-lg-block">
+      <div className="d-none d-xl-block">
         <Link to="/" className="navbar-brand ms-2">
           <Icon icon="spartav2" className="my-auto" size="35" />
           <h4 className="d-none d-sm-inline ms-2">Spartan Protocol</h4>
