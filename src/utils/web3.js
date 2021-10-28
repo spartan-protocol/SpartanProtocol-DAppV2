fc3eaf2844838dd25c8299759153433fee4df089
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-10-28 11:34:53 +0800

    updated packages

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -192,7 +192,7 @@ export const bscRpcsMN = [
   'https://bsc-dataseed.binance.org/',
   'https://bsc-dataseed1.defibit.io/',
   'https://bsc-dataseed1.ninicoin.io/',
-  // BACKUPS BELOW
+  // // BACKUPS BELOW
   'https://bsc-dataseed2.defibit.io/',
   'https://bsc-dataseed3.defibit.io/',
   'https://bsc-dataseed4.defibit.io/',
