dea53a2f7615067b72d34acce38a01fc15bea271
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-12-10 14:40:06 +0800

    Revert "comment out troublesome rpc"
    This reverts commit d78d2c7edb83a4f3098c2402867cbb3e19decc0a.

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -203,7 +203,7 @@ export const bscRpcsMN = [
   'https://bsc-dataseed2.binance.org/',
   'https://bsc-dataseed3.binance.org/',
   'https://bsc-dataseed4.binance.org/',
-  // 'https://binance.ankr.com/', // Causing holdup in Promise.allSettled()
+  'https://binance.ankr.com/',
 ]
 
 export const deadAddress = '0x000000000000000000000000000000000000dEaD'
