1ad0ce3129675c621fe7ff5d0c2c2016b8cd27ba
Samus <26967164+SamusElderg@users.noreply.github.com>
2022-03-14 15:56:11 +0800

    rip out stylesheets
    closes #729

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import './assets/css/spartan-icons.css'
-import './assets/scss/spartan.scss'
 import Providers from './Providers'
 
 ReactDOM.render(<Providers />, document.getElementById('root'))
