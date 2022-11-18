5d8c519e5b13f2730b13ce61374e1b9d5641f54c
Samus <26967164+SamusElderg@users.noreply.github.com>
2022-11-18 12:07:19 +0800

    some changes to the light/dark mode logic
    - set the local isDark default state to `true` (DApp theme will be dark for all users by default)
    - added back in persistent user state for theme preference (this is priority #1) but changed the var to a simple `lightMode` style var in localStorage
    - moved the new system state logic to priority #2 and had it use an interfaced toggle logic without affecting persistent state in localStorage
    - for now it always results in dark mode unless the user specifically changes to light mode, in which case it goes into localStorage and persists as their preference from there-on
    - the commented-out logic we can uncomment later when we fix the light theme styling and have the behavior select light or dark theme for the user based on system preferences *as long as the user has not manually toggled light/dark into localStorage* this will require another change to the localStorage var name/logic (by design, to force the user state to go from a clean slate with any future changes)

--- src/providers/Theme/index.js
+++ src/providers/Theme/index.js
@@ -1,38 +1,49 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
 
 const ThemeContext = createContext()
-const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+// const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)') // Un-comment this line when/if we update the light theming/style to a better standard
+
 const ThemeProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(true) // Dark theme by default
+
+  // On manual theme mode toggle (persistent state for user)
+  const toggleDark = () => {
+    if (isDark) {
+      setIsDark(false)
+      document.body.classList.toggle('dark-theme', false)
+      window.localStorage.setItem('sp-lightMode', true) // we should only update localStorage when user specifically selects the theme
+    } else {
+      setIsDark(true)
+      document.body.classList.toggle('dark-theme', true)
+      window.localStorage.removeItem('sp-lightMode') // we should only update localStorage when user specifically selects the theme
+    }
+  }
+
+  // On theme toggle (non-persistent state for user)
+  const toggleIsDarkOnly = (bool) => {
+    setIsDark(bool)
+    document.body.classList.toggle('dark-theme', bool)
+  }
 
   // On load
   useEffect(() => {
-    /* Code below can be used wether to check if user changes his prefered system-theme mid-use Dapp
-     darkThemeMq.addEventListener('change', (e) => {
-      if (e.matches) {
-        setIsDark(true)
-        document.body.classList.toggle('dark-theme', true)
-      } else {
-        setIsDark(false)
-        document.body.classList.toggle('dark-theme', false)
-      }
-    }) */
-    if (!darkThemeMq.matches) {
-      setIsDark(true)
-      document.body.classList.toggle('dark-theme', true)
+    if (window.localStorage.getItem('sp-lightMode')) {
+      // 1st priority: Persistent state from localStorage (ie. the user's specified preference)
+      toggleIsDarkOnly(false) // If user previously selected light mode as their preference
+      // We dont need to check for 'dark' preference as thats the default/fallback
     } else {
-      // Do it anyway -> Theme Color could be used later
-      setIsDark(true)
-      document.body.classList.toggle('dark-theme', true)
+      // If the user has not specified a preference into localStorage, fall back to conditional logic
+      // 2nd priority: system dark theme
+      // toggleDarkOnly(darkThemeMq.matches) // Un-comment this line when/if we update the light theming/style to a better standard
+      toggleIsDarkOnly(true) // Delete this line when/if we update the light theming/style to a better standard
+
+      /* Listen for changes to user's system-theme after the initial load
+      Uncomment below when/if we update the light theming/style to a better standard */
+      // darkThemeMq.addEventListener('change', (e) => {
+      //   toggleIsDarkOnly(e.matches)
+      // })
     }
   }, [])
-
-  // On toggle
-  const toggleDark = () => {
-    document.body.classList.toggle('dark-theme', !isDark)
-    window.localStorage.setItem('theme', !isDark)
-    setIsDark(!isDark)
-  }
 
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
