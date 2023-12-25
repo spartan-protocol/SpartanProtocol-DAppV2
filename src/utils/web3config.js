fff6acd027b3deb813df8fd4c1b8194b18d3da37
Samus <26967164+SamusElderg@users.noreply.github.com>
2023-12-25 15:34:52 +0800

    remaining wagmi migrations
    - wagmi `createClient` -> `createConfig`

--- src/utils/web3config.js
+++ src/utils/web3config.js
@@ -1,4 +1,4 @@
-import { configureChains, createClient } from 'wagmi'
+import { configureChains, createConfig } from 'wagmi'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { bsc, bscTestnet } from 'wagmi/chains'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
@@ -97,7 +97,7 @@ export const connectorsByName = (connectorName, connectorsList) => {
   return connectorsList[0] // Fallback to injected
 }
 
-export const wagmiClient = createClient({
+export const wagmiClient = createConfig({
   connectors,
   provider,
 })
