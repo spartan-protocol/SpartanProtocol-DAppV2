f62dd7180e66aef5d5d22d0f04581d56ebbe9ae3
Samus <26967164+SamusElderg@users.noreply.github.com>
2024-01-04 14:48:17 +0800

    more cleanup
    - added some extra css hashes to the csp + google fonts
    - added some margin to the friends tile headers
    - aligned text center on the friends tiles
    - removed some of the older less relevant friends from the list

--- src/containers/Friends/types.js
+++ src/containers/Friends/types.js
@@ -1,13 +1,5 @@
 export const friends = [
   {
-    name: 'Immunefi',
-    logo: 'immunefiText',
-    desc: 'The leading web3 bug bounty platform, protecting $100 billion in user funds.',
-    providing: 'Smart contract bug bounty',
-    linkText: ['View Bounty'],
-    linkUrl: ['https://www.immunefi.com/bounty/spartanprotocol'],
-  },
-  {
     name: 'Code4rena',
     logo: 'c4Text',
     desc: 'C4 audit contests find more bugs faster than any other method.',
@@ -64,21 +56,5 @@ export const friends = [
     linkUrl: [
       'https://app.bubblemaps.io/bsc/token/0x3910db0600ea925f63c36ddb1351ab6e2c6eb102',
     ],
-  },
-  {
-    name: 'OntoWallet',
-    logo: 'ontoText',
-    desc: 'Decentralized, cross-chain wallet, for users to securely manage identities, & digital assets.',
-    providing: 'SPARTA token & DApp whitelisting',
-    linkText: ['ONTO Wallet'],
-    linkUrl: ['https://onto.app'],
-  },
-  {
-    name: '4thTech',
-    logo: 'fourText',
-    desc: 'Send/receive dMails & on-chain messages with no ads, data mining nor tracking.',
-    providing: 'Community portal for dMail & dChat',
-    linkText: ['View Portal', 'View 4thTech'],
-    linkUrl: ['https://spartan.4thtech.io/', 'https://4thtech.io/'],
   },
 ]
